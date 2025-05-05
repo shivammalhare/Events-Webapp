94fb7e17dcc977b6ab2b6e8315038c7716c88e82
Shivam <shivam@rezoomex.com>
2025-05-05 14:25:16 +0530

    Fix: use env variable for Mongo URI

--- event_backend/db.py
+++ event_backend/db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URI = "mongodb+srv://<db_username>:<db_password>@rexcluster.jp4vrde.mongodb.net/?retryWrites=true&w=majority&appName=RexCluster"
+MONGO_URI = os.environ.get("MONGO_URI")  # ✅ Loads it from Render
+
 client = MongoClient(MONGO_URI)
 db = client["eventdb"]
 events_collection = db["events"]
