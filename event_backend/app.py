2772a303353e7766991e1ba1dd339cce6b86a1f6
Shivam <shivam@rezoomex.com>
2025-05-05 16:42:04 +0530

    Fix:Added CORS to Backend

--- event_backend/app.py
+++ event_backend/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # ✅ NEW
 from db import events_collection
 from utils import generate_event_id
 
 app = Flask(__name__)
+CORS(app)  # ✅ Allow all origins during dev
+
+@app.route('/events', methods=['GET'])
+def get_events():
+    events = list(events_collection.find({}, {"_id": 0}))
+    return jsonify(events), 200
 
 @app.route('/events', methods=['POST'])
 def create_event():
@@ -10,11 +17,3 @@ def create_event():
     data['event_id'] = generate_event_id()
     events_collection.insert_one(data)
     return jsonify({"message": "Event created", "event_id": data['event_id']}), 201
-
-@app.route('/events', methods=['GET'])
-def get_events():
-    events = list(events_collection.find({}, {"_id": 0}))  # Exclude Mongo's _id
-    return jsonify(events), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
